84f156c2b2287192544416ab23b6cce41c4329d1
Gurparmvir Singh <43221967+gurparmvir@users.noreply.github.com>
2020-11-12 22:52:33 -0500

    Add files via upload
    In this shopping list I have used the DOM callback functions. User can add the items to the shopping list and remove the item from the list. When user hit enter or click enter the item will add to the end of the list and when user click delete it will delete the item from the top of the list.

---
+++ script.js
@@ -0,0 +1,40 @@
+var button = document.getElementById("enter");
+var button1 = document.getElementById("delete");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var list = document.getElementById("myList");  
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function delteListElement(){
+	
+	list.removeChild(list.lastElementChild);  
+}
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+button1.addEventListener("click", delteListElement);
